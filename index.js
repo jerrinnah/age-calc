2262a23b4a2660053b4504ae3fd4a18171337a90
NEJ <devjerrynnah@gmail.com>
2023-04-05 09:17:36 +0100

    fixed styling

--- index.js
+++ index.js
@@ -25,10 +25,20 @@ monthsInput.addEventListener('input', function () {
 monthsInput.addEventListener("input", function(){
     if (value > 4){
         value = value.slice(0, 4)
-
     }
 })
 
-formBtn.addEventListener("click", ()=> {
+formBtn.addEventListener("click", () => {
     let setDay = daysInput.value
+    let setMonth = monthsInput.value - 1
+    let setYear = yearsInput.value
+    let now = new Date()
+    let newDate = Date(setYear, setMonth, setDay)
+    const ageInMiliseconds = now - newDate
+    const milisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+    const milisecondsInMonth = 1000 * 60 * 60 * 24 * 30.44;
+    const months = Math.floor((remainingMiliseconds % milisecondsInMonth) / (1000 * 60 * 60 * 24));
+    
+
 })
+
